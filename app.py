da5f58f13b9d9562904ba1b1d3ad83670cf93bef
Rodrigo Maffei <rodrigoa.maffei@gmail.com>
2024-02-18 19:55:08 -0300

    routes finish

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 from flask import Flask, request
+from src.fetch_pdb import phi_psi
+from src.plot import plot
 from src.functions import allowed_file, extractFileByArchive
 
 app = Flask(__name__)
@@ -31,3 +33,16 @@ def uploadAndApply():
 
     return {'msg': 'Archivos cargados con éxito'}
 
+@app.route("/pdb", methods=["POST"])
+def get_pdb_proceed():
+    file = request.files['file_pdb']
+    proceed = phi_psi(file)
+    print(proceed)
+    return {'msg': 'Éxito', 'data': proceed}
+
+@app.route("/plot", methods=["POST"])
+def get_plot():
+    file = request.files['file_pdb']
+    plot_proceed = plot(file)
+    print(plot_proceed)
+    return {'msg': 'Éxito', 'data': plot_proceed}

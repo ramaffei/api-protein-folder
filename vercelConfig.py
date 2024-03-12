22a2fe881885398397acc3fd128ce4d160de605b
Rodrigo Maffei <rodrigoa.maffei@gmail.com>
2024-03-11 22:30:05 -0300

    va queriendo

--- vercelConfig.py
+++ vercelConfig.py
@@ -2,4 +2,7 @@ from os.path import abspath, dirname
 
 # Define the application directory
 BASE_DIR = dirname(dirname(abspath(__file__)))
+PROPAGATE_EXCEPTIONS = True
+ERROR_404_HELP = False
+DEBUG = True
 UPLOAD_FOLDER = 'upload'
